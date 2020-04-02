fa35e3d35fbafcc53a8f9d1d9b6419ad064f41fc
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-04-02 16:24:20 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/santosediego/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.santosediego.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.santosediego.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
